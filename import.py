a35acf21e585544d93e60df8a035145b3c4e9426
James Cleveland <jamescleveland@gmail.com>
2010-02-16 20:46:26 +0000

    Added template, made script output XML

--- import.py
+++ import.py
@@ -1,6 +1,36 @@
+#!python
 import csv
+import pprint
+from xml.dom.minidom import parse
 
-contacts = csv.DictReader(
+pp = pprint
+
+doc = parse( 'template.xml' ) # parse an XML file by name
+root = doc.getElementsByTagName( "root" )
+for node in root:
+	root = node
+#print doc.toprettyxml(indent="  ")
+#root = doc.createElement( "root" )
+#doc.appendChild( root )
+
+
+#info = doc.createElement("Info")
+#root.appendChild(info)
+#info.appendChild(
+#	doc.createElement( "Location" ) ).appendChild(
+#		doc.createTextNode( "1" ) )
+#info.appendChild(
+#	doc.createElement( "Version" ) ).appendChild(
+#		doc.createTextNode( "3" ) )
+#info.appendChild(
+#	doc.createElement( "PB" ) ).appendChild(
+#		doc.createTextNode( "true" ) )
+#info.appendChild(
+#	doc.createElement( "SMS" ) ).appendChild(
+#		doc.createTextNode( "false" ) )
+
+
+csvContacts = csv.DictReader(
 	open( "contacts.csv" ),
 	delimiter = ",",
 	quotechar = '"'
@@ -44,26 +74,32 @@ map = {
 	"szCompany" 		: "Company", \
 }
 
-mapped	= []
+xmlContacts	= []
 
-for contact in contacts:
-	currMappedItem = necessary
+for csvContact in csvContacts:
+	childInfo = doc.createElement("ChildInfo")
+	childInfo.setAttribute( "DataType", "PB" )
+	xmlContact = necessary
 	for k, v in map.items():
 		if type( v ) == type( [] ):
 			s = []
 			for f in v:
-				if( len( contact[ f ] ) > 0 ):
-					s.append( contact[ f ] )
-			currMappedItem[ k ] = " ".join( s )
+				if( len( csvContact[ f ] ) > 0 ):
+					s.append( csvContact[ f ] )
+			xmlContact[ k ] = " ".join( s )
 		else:
-			currMappedItem[ k ] = contact[ v ]
-	print currMappedItem
-	mapped.append( currMappedItem )
+			xmlContact[ k ] = csvContact[ v ]
+	for k, v in xmlContact.items():
+		if len( "%s" % v ) > 0:
+			childInfo.appendChild(
+				doc.createElement( k ) ).appendChild(
+					doc.createTextNode( "%s" % v ) )
+		else:
+			childInfo.appendChild(
+				doc.createElement( k ) )
+			
+	root.appendChild( childInfo )
+	
 
-currMappedItem = []
-mapped.append( currMappedItem )
 
-print "\
-\
-"
-print mapped
+print doc.toprettyxml(indent="  ")
